1efb976168077d6c113663f9464c4fbe8dcca6cd
Camilo Uribe Diaz <milouri23@gmail.com>
2025-05-11 00:07:56 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+
+function getTimestamp() {
+  return Date.now();
+}
+
+function getLongTime(locale = 'es-ES') {
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    timeZoneName: 'short'
+  };
+
+  return new Date().toLocaleDateString(locale, options)
+}
+
+module.exports = {
+  getTimestamp,
+  getLongTime
+}
